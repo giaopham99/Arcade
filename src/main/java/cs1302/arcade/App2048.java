9e4ecb96dd8d4981675a8f8946354397b608b6ba
Joshua Drexler (Josh) <jpd67704@uga.edu>
2019-04-13 15:06:55 -0400

    Minor tweaks to Utility bar and planned for 2048

---
+++ src/main/java/cs1302/arcade/App2048.java
@@ -0,0 +1,17 @@
+package cs1302.arcade;
+
+public class App2048 extends Scene{
+
+    //player class to hold score
+    //tile class to hold tile values and images
+    //some checker class to check the way you can move
+    
+    public App2048(){
+        super();
+        //need a tile array
+        //scorekeeper
+        //win menu (continue or new game)
+        //game over menu (retrun to arcade, close, or new game)
+    }//App2048
+    
+}//App2048
--- src/main/java/cs1302/arcade/UtilityBar.java
+++ src/main/java/cs1302/arcade/UtilityBar.java
@@ -6,6 +6,7 @@ import javafx.scene.control.MenuBar;
 import javafx.scene.control.Button;
 import javafx.scene.control.Tab;
 import javafx.scene.control.TabPane;
+import javafx.scene.control.TabPane.TabClosingPolicy;
 import javafx.scene.layout.VBox;
 import javafx.scene.text.Text;
 import javafx.scene.Scene;
@@ -21,21 +22,21 @@ public class UtilityBar extends MenuBar{
     MenuItem controls;
     Menu file;
     Menu help;
-    private final String RULES_TETRIS = "Controls- \n" +
+    private final String RULES_TETRIS = "Controls- \n\n" +
             "LEFT ARROW KEY: Moves the block left one space. \n" +
             "RIGHT ARROW KEY: Moves the block right one space. \n" +
             "DOWN ARROW KEY: Slams the block down." +
-            "SPACE BAR: Rotates block. \n \n" +
+            "SPACE BAR: Rotates block. \n \n \n" +
             "How To Play- \n" +
             "Fill an entire row with blocks to increase \n score and clear the row. " +
             "If the blocks exceed the maximum height of the play area,\n the game is over.";
 
-    private final String RULES_2048 = "Controls- \n" +
+    private final String RULES_2048 = "Controls- \n \n" +
             "LEFT ARROW KEY: Slides all Tiles to the left. \n" +
             "RIGHT ARROW KEY: Slides all Tiles to the right. \n" +
             "DOWN ARROW KEY: Slides all Tiles down. \n"+
-            "UP ARROW KEY: Slides all Tiles up. \n"+
-            "How To Play- \n"+
+            "UP ARROW KEY: Slides all Tiles up. \n \n"+
+            "How To Play- \n \n"+
             "Combine tiles with the same numeric values to reach 2048. \n"+
             "After reaching the Tile 2048, you may choose to continue to reach 8192.";
     
@@ -60,16 +61,18 @@ public class UtilityBar extends MenuBar{
     }//UtilityBar
 
     private void controlScreen(){
-        Button close = new Button("Close");
+        Button close1 = new Button("Close");
+        Button close2 = new Button("Close");
         VBox pageTetris = new VBox();
-        pageTetris.getChildren().addAll(new Text(RULES_TETRIS), close);
+        pageTetris.getChildren().addAll(new Text(RULES_TETRIS), close1);
         VBox page2048 = new VBox();
-        page2048.getChildren().addAll(new Text(RULES_2048), close);
+        page2048.getChildren().addAll(new Text(RULES_2048), close2);
         
         TabPane pane = new TabPane();
         Tab tabTetris = new Tab("Tetris Rules",pageTetris);
         Tab tab2048 = new Tab("2048 Rules", page2048);
 
+        pane.setTabClosingPolicy(TabClosingPolicy.UNAVAILABLE);
         pane.getTabs().addAll(tabTetris, tab2048);
    
         //Create Stage and Scene
@@ -77,7 +80,8 @@ public class UtilityBar extends MenuBar{
         Stage helpWindow = new Stage();
         helpWindow.setScene(scene);
 
-        close.setOnAction(e -> helpWindow.close());
+        close1.setOnAction(e -> helpWindow.close());
+        close2.setOnAction(e -> helpWindow.close());
         
         helpWindow.initModality(Modality.APPLICATION_MODAL);
         helpWindow.setTitle("How To Play");
