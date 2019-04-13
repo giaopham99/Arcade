96408145c583c0abcae7531a5b397d6dca8f2953
Joshua Drexler (Josh) <jpd67704@uga.edu>
2019-04-13 14:33:33 -0400

    Debugged Main Menu

--- src/main/java/cs1302/arcade/MainMenu.java
+++ src/main/java/cs1302/arcade/MainMenu.java
@@ -6,8 +6,10 @@ import javafx.scene.layout.VBox;
 import javafx.scene.layout.HBox;
 import javafx.scene.text.Text;//
 import javafx.scene.layout.StackPane;
+import javafx.scene.layout.FlowPane;
 import javafx.scene.Scene;
 import javafx.geometry.Pos;
+import javafx.geometry.Orientation;
 
 public class MainMenu extends VBox{
 
@@ -16,22 +18,27 @@ public class MainMenu extends VBox{
     ImageView viewTetris;
     ImageView view2048;
     ImageView background;
-    HBox imageBox;
+    FlowPane imagePane;
     
     StackPane stack;
     //special toolbar
 
     public MainMenu(){
    
-        imageBox=new HBox();
+        imagePane=new FlowPane(Orientation.HORIZONTAL);
         stack=new StackPane();
         viewTetris=new ImageView(new Image("MenuImages/TetrisCover.jpeg"));
+        viewTetris.setFitWidth(220);
+        viewTetris.setFitHeight(220);
         view2048=new ImageView(new Image("MenuImages/2048Cover.png"));
         background=new ImageView(new Image("MenuImages/Background.jpg"));
-        imageBox.getChildren().addAll(viewTetris, view2048);
-        stack.setAlignment(imageBox, Pos.BOTTOM_CENTER);
-        stack.getChildren().addAll(background, imageBox);
-        this.getChildren().addAll(new UtilityBar(), imageBox);
+        imagePane.setHgap(100);
+        imagePane.setAlignment(Pos.BOTTOM_CENTER);
+        imagePane.getChildren().addAll(viewTetris, view2048);
+        
+        stack.getChildren().addAll(background, imagePane);
+
+        this.getChildren().addAll(new UtilityBar(), stack);
     }//MainMenu
     
 }//MainMenu
