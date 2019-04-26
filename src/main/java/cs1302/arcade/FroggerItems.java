43db82ebcf6f808f88751f3a41027f2c3d631c7e
Giao Pham <gpp60496@uga.edu>
2019-04-26 16:21:00 -0400

    created FroggerItems class

---
+++ src/main/java/cs1302/arcade/FroggerItems.java
@@ -0,0 +1,11 @@
+package cs1302.arcade;
+
+public class FroggerItems{
+    /*
+      hold image, x, y, boundaries(max x,y),disable
+      ----methods
+      set x y
+      switch boundaries once reach the midpoint
+
+     */
+}//FroggerItems
